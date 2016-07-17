30dbd61c98f1ada16e53a06dd1de1ff3f6a4e62a
Leyth <mawehelie@gmail.com>
2016-07-16 20:04:02 -0700

    clear input

--- js/app.js
+++ js/app.js
@@ -2,16 +2,27 @@ function init() {
 
 	var username = document.getElementById('name');
 	var userid = username.value;
-	console.log(userid);
-	var url = 'https://api.github.com/users/'+userid;
-	var xhr = new XMLHttpRequest();
-	xhr.onload = function (e) {
-		var data = JSON.parse(xhr.responseText);
-		console.log(data.name);
-	}
-	xhr.open("GET", url);
-	xhr.send();
+	if (userid == '') {
+		console.log('empty string');
+	} else { 
+			console.log(userid);
+			var url = 'https://api.github.com/users/'+userid;
+			var xhr = new XMLHttpRequest();
+			xhr.onload = function (e) {
+				var data = JSON.parse(xhr.responseText);
+				console.log(data.name);
+			}
+			
+			xhr.open("GET", url);
+			xhr.send();
 
+			// clear input
+			function reset() {
+				document.getElementById('name').value = '';
+			}
+
+			reset();
+		}
 
 }
 
